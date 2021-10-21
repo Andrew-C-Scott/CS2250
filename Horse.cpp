6e32ac1d0b83aa13d1b022e225d53003ccf70ee6
Andrew-C-Scott <36336444+Andrew-C-Scott@users.noreply.github.com>
2021-10-21 18:54:57 -0500

    Revert "Laptop version"

--- Horse.cpp
+++ Horse.cpp
@@ -4,12 +4,3 @@
 using namespace std;
 
 
-//**********************************
-// Horse member function definitions
-//**********************************
-
-
-int runASecond() // method that adds to distance traveled an amount from 0 - maxRunDistPerSecond (this moves horse along track)
-{
-	
-}
--- Horse.h
+++ Horse.h
@@ -5,12 +5,8 @@
 #include <stdlib.h>
 using namespace std;
 
-//**********************************************************
-//Class imlementation file, contains class members/functions
-//**********************************************************
-
 class Horse
-{ 
+{
 private:
 	string name; //horse name
 	string rider; // rider name
@@ -18,8 +14,6 @@ private:
 	int distanceTraveled; // how far horse has gone already
 	int racesWon; //number of races won by horse and rider
 public:
-	Horse() //default constructor
-	{}
 	Horse(string horseName, string riderName) //constructor accepts horse/rider name as argument. Initialize each horse to a random maxRunDistperSecond
 	{
 		name = horseName;
@@ -29,26 +23,13 @@ public:
 	}
 	string getHorseName() //returns horse name
 	{
-		cout << "Enter Horse name: " << endl;
-		cin >> name;
-		
 		return name;
 	}
 	string getRiderName() //returns rider name
 	{
-		cout << "Enter rider name: " << endl;
-		cin >> rider;
-
 		return rider;
 	}
-	
-	
-	
-	int runASecond(int distTravel); // method that adds to distance traveled an amount from 0 - maxRunDistPerSecond (this moves horse along track)
-
-
-	int sendToGate();// sends horse to the start of a race by setting distance traveled to 0
-	int displayHorse(int goalLength);
 };
+
 
 

5102d7ad84f87f12eb8a6ca45001cb2bf79d3e30
Andrew-C-Scott <36336444+Andrew-C-Scott@users.noreply.github.com>
2021-10-23 21:12:59 -0500

    Update CS2250 Project 3.cpp

--- CS2250 Project 3.cpp
+++ CS2250 Project 3.cpp
@@ -9,6 +9,7 @@
 #include <cstdlib>
 #include <time.h>
 #include "Horse.h"
+
 using namespace std;
 
 //*****************************
@@ -23,30 +24,54 @@ using namespace std;
 
 int main()
 {
-	Horse *horseObj = nullptr;
+	Horse* horseObj = nullptr;
 	int numObj = 0;
 
 	//validate entry for number of horses/riders must accept numObj > 0
-	do{
+	do {
 		cout << "Enter Number of Horses are in the race: " << endl;
 		cin >> numObj;
-	}while (numObj <= 0);
+	} while (numObj <= 0);
 
-	horseObj = new Horse[numObj];
+	horseObj = new Horse[numObj]; //dynamic array of size num obj (type horse)
 
 	for (int i = 0; i < numObj; i++) // loop through number of objects assign horse/rider name;
 	{
-		cout << "Horse " << i + 1 << ": " << horseObj[i].getHorseName() << " riden by, " 
-		<< horseObj[i].getRiderName() << "." << endl;
-		
+		cout << "Horse " << i + 1 << ": " << horseObj[i].getHorseName() << " riden by, "
+			<< horseObj[i].getRiderName() << "." << endl;
+
 		cout << "\n";
 	}
-
 	//validate entry for race distance must be greater than 100 yards
 	int raceDist;
 	do {
 		cout << "Pick Race distance, must be greater than 100 yards: " << endl;
 		cin >> raceDist;
 	} while (raceDist <= 100);
+
+	char runASec; // hold Y or N
+	cout << "Do you want to begin the race?" << "Enter Y for Yes, N for No. " << endl;
+	cin >> runASec;
+
+	if (tolower(runASec) == 'y')
+	{
+		cout << "Race will begin now" << endl;
+	}
+	else if (tolower(runASec) == 'n')
+	{
+		cout << "Please rerun application to begin again : " << endl;
+		return 0;
+	}
+	else
+	{
+		cout << "Please enter valid choice or press q to quit" << endl;
+		cin >> runASec;
+		if (runASec == 'q')
+		{
+			return 0;
+		}
+	}
+
+	
 
 }
